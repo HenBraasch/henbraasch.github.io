bee07d4e512303f7fcfab41c6e57ad8e79e96cb7
Hendrik Braasch <dev@hendrikbraasch.com>
2022-08-12 20:22:38 +1200

    Replace old app with new vue app

---
+++ docs/js/about.e4ec2e80.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkhendrik_braasch_website"]=self["webpackChunkhendrik_braasch_website"]||[]).push([[443],{381:function(e,n,a){a.r(n),a.d(n,{default:function(){return o}});var r=a(396);const s={class:"about"},t=(0,r._)("h1",null,"This is an about page",-1),u=[t];function i(e,n){return(0,r.wg)(),(0,r.iD)("div",s,u)}var c=a(89);const h={},b=(0,c.Z)(h,[["render",i]]);var o=b}}]);
+//# sourceMappingURL=about.e4ec2e80.js.map
--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,6 @@
     <header class="mb-auto">
       <nav class="nav nav-masthead justify-content-center float-md-end">
         <router-link to="/" class="nav-link">Home</router-link>
-        <router-link to="/about" class="nav-link">About</router-link>
       </nav>
     </header>
     <router-view/>
