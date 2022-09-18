a1d8a7d4f17eec50817e4cbb1205e4b7d8024457
Hendrik Braasch <dev@hendrikbraasch.com>
2022-09-18 21:21:20 +1200

    Add component to select locale

---
+++ src/components/SelectLocale.vue
@@ -0,0 +1,19 @@
+<template>
+    <nav>
+      <ul class="nav nav-masthead justify-content-center">
+        <li v-for="(lang, i) in langs"
+          :key="`lang-${i}`"
+          :value="lang.short"
+          class="nav-link"
+          v-on:click="$i18n.locale = lang.short">{{lang.full}}</li>
+      </ul>
+    </nav>
+</template>
+<script>
+export default {
+  name: 'SelectLocale',
+  data () {
+    return { langs: [{ full: 'English', short: 'en' }, { full: 'Deutsch', short: 'de' }] }
+  }
+}
+</script>
